5a86cd6ebf0b72cff0397d1bb799bbcaae66cfbb
SergioMontoro <84936651+SergioMontoro@users.noreply.github.com>
2024-03-28 06:50:39 +0000

    Commit para entrega d proyecto

--- src/models.py
+++ src/models.py
@@ -7,23 +7,50 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
+    
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    lastname = Column(String(250))
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    favourites = relationship('Favourite', backref='user', lazy=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Favourite(Base):
+    __tablename__ = 'favourite'
+  
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    character_id = Column(Integer, ForeignKey('character.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id'))
+
+class Character(Base):
+    __tablename__ = 'character'
+ 
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    eye_color = Column(String(250))
+    hair_color = Column(String(250))
+    favourites = relationship('Favourite', backref='character', lazy=True)
+
+class Planet(Base):
+    __tablename__ = 'planet'
+ 
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    population = Column(String(250))
+    favourites = relationship('Favourite', backref='planet', lazy=True)
+
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    model = Column(String(250))
+    favourites = relationship('Favourite', backref='vehicle', lazy=True)
+
 
     def to_dict(self):
         return {}
